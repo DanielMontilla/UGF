a45af7bbeff0cf353098d46b1c3225227ae62f7d
Daniel Montilla <montilla.d2000@gmail.com>
2023-04-20 21:21:31 -0400

    completed rectangle pipeline`

--- examples/template/src/main.ts
+++ examples/template/src/main.ts
@@ -1,9 +1,14 @@
-import { Surface } from 'ugf';
+import { Surface, RectangleComponent, Vec2, Color } from 'ugf';
 
-class Game extends Surface {
-
-}
+class Game extends Surface {}
 
 const game = new Game();
-
 game.mount(document.getElementById('app')!);
+
+const rect = new RectangleComponent({
+  color: new Color(0.7, 0.2, 0.5),
+  position: new Vec2(10, 10),
+  size: Vec2.all(64),
+});
+
+game.add(rect);
